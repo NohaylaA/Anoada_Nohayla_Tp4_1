faba3681456a8149bc5989fca62bfab828f36995
nohayla <anoada.nohayla2001@gmail.com>
2024-06-01 02:49:55 +0100

    last

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -3,7 +3,14 @@ import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
 import { provideClientHydration } from '@angular/platform-browser';
+import {HTTP_INTERCEPTORS, provideHttpClient, withFetch} from "@angular/common/http";
+import {AppHttpInterceptor} from "./services/app-http.interceptor";
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideRouter(routes), provideClientHydration()]
+  providers: [
+    provideRouter(routes),
+    provideClientHydration(),
+    provideHttpClient(withFetch()),
+    {provide : HTTP_INTERCEPTORS, useClass : AppHttpInterceptor, multi : true}
+  ]
 };
